bc2688d47cf85d24367105e72e7db58c6ce8035b
itsglobally <itsglobally1324@gmail.com>
2025-06-25 09:40:26 +0000

    awa

--- src/main/java/me/itsglobally/addon/ffa/CritArrowMain.java
+++ src/main/java/me/itsglobally/addon/ffa/CritArrowMain.java
@@ -2,14 +2,19 @@ package me.itsglobally.addon.ffa;
 
 import me.bedtwL.ffa.api.EffectAddon;
 
-public abstract class CritArrowMain implements EffectAddon {
+import java.util.UUID;
+
+public class CritArrowMain implements EffectAddon {
     @Override
     public Integer getAPIVer() { return 1; };
     @Override
     public String getName() {
         return "CritArrow";
     }
-
+    @Override
+    public UUID authorUUID() {
+        return UUID.fromString("748c3863-c04d-493b-bce3-05a1765a5b56");
+    }
     @Override
     public String getAuthor() {
         return "ItsGlobally";
