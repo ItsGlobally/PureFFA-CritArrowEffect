21e264018a2eb5b47a42ca066cf59b7c7aaf1fe6
itsglobally <itsglobally1324@gmail.com>
2025-06-25 09:29:44 +0000

    awa

--- src/main/java/me/itsglobally/addon/ffa/CritArrowMain.java
+++ src/main/java/me/itsglobally/addon/ffa/CritArrowMain.java
@@ -4,6 +4,8 @@ import me.bedtwL.ffa.api.EffectAddon;
 
 public abstract class CritArrowMain implements EffectAddon {
     @Override
+    public Integer getAPIVer() { return 1; };
+    @Override
     public String getName() {
         return "CritArrow";
     }
